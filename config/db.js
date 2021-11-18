b2fea614b4a51870cbe44be1991a3ec708f96b74
MeKa <k4ntarmetin@gmail.com>
2021-11-18 21:09:22 +0300

    TahminWorld Api DB BAGLANTISI

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+const DB_BAGLANTI = "mongodb+srv://root:root47tahmin@cluster0.xruvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const dbBaglanti = async () => {
   await mongoose
-    .connect(process.env.DB_BAGLANTI, {
+    .connect(DB_BAGLANTI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
